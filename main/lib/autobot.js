20edd70f49c72de7eb1326d45cdbfb3ad3d368a8
Alan Andrade <alan.andradec@gmail.com>
2016-07-08 15:28:46 -0700

    punctuation little

--- main/lib/autobot.js
+++ main/lib/autobot.js
@@ -9,7 +9,7 @@ module.exports.drawTasks = function (tasks) {
     for (var i = 0, l = instructions.length; i < l; i ++) {
         lines.push(Drawer.draw(instructions[i], tasks));
     }
-    lines.push("===============   Legend   ===============");
-    lines.push(Drawer.BLOCKER + " Blocker " + Drawer.PARTIAL + "Partially done, but not a blocker " + Drawer.DONE + "Done");
+    lines.push(".\n===============   Legend   ===============");
+    lines.push(Drawer.BLOCKER + " Blocker   " + Drawer.PARTIAL + " Partially done, but not a blocker  " + Drawer.DONE + "  Done");
     return lines.join("\n");
 };
