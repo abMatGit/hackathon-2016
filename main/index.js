047e2d2510120c9405b3d071f2a19e3d6e6a438c
abMatGit <andymatuszewski@gmail.com>
2016-07-12 01:52:36 -0700

    we are using ADAPTERS not INTERFACES!

--- main/index.js
+++ main/index.js
@@ -2,17 +2,14 @@
 
 var program = require('commander');
 var fixtureTasks = require('./fixtures/hardcoded_tasks');
-var autobotCLI = require('./lib/interfaces/command_line');
+var autobotAdapter = require('./lib/adapters/command_line');
 
-var cmdValue;
+var params = {};
 
 program
-  .arguments('<status>')
+  .arguments('<command>')
   .action(function(cmd) {
-    cmdValue = cmd;
+    autobotAdapter.processCommand(cmd, params);
   })
   .parse(process.argv);
 
-if (cmdValue == 'status') {
-    autobotCLI.status({ tasks: fixtureTasks.hardCodedTasks});
-}
