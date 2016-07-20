1e89fe0ec1ec930d0739b3238f9901086160c1c8
abMatGit <andymatuszewski@gmail.com>
2016-07-20 07:21:04 -0700

    add JIRA api client

---
+++ main/autobot/project_trackers/jira.js
@@ -0,0 +1,25 @@
+var credentials = require('../../../configs/jira_credentials');
+var JiraApi = require('jira').JiraApi;
+var config = {
+  protocol: 'https',
+  host: 'lumoslabs.atlassian.net',
+  port: null,
+  username: credentials.username, // your jira username
+  password: credentials.password, // your jira password
+  api_version: 'latest'
+}
+
+module.exports.client = new JiraApi(config.protocol,
+                                    config.host,
+                                    config.port,
+                                    config.username,
+                                    config.password,
+                                    config.api_version);
+
+// Call this like:
+// var jira_client = require('project_trackers/jira').client;
+// var jira_callback = require('project_trackers/jira').status_issue_callback;
+//
+// Invoke it with:
+// jira_client.findIssue('IOS-1697', jira_callback);
+module.exports.status_issue_callback = function(err, issue) { console.log('Status: ' + issue.fields.status.name) };
