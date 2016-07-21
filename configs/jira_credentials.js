16e0ee1e904175f59e8682fe448462456b77f5ac
Alan Andrade <alan.andradec@gmail.com>
2016-07-20 23:51:17 -0700

    Fixes config for jira

--- configs/jira_credentials.js
+++ configs/jira_credentials.js
@@ -1,5 +1,7 @@
 module.exports = {
+    host: 'host',
+    protocol: null,
+    api_version: 'latest'
     username: 'your_username',
-    password: 'password',
-    host: 'host'
+    password: 'password'
 }
--- main/autobot/project_trackers/jira.js
+++ main/autobot/project_trackers/jira.js
@@ -3,14 +3,13 @@ var config = require('../../../configs/jira_credentials');
 var ProjectTracker = require('../autobot/project_trackers/project_tracker');
 var JiraApi = require('jira').JiraApi;
 
-
 var Jira = Object.create(ProjectTracker);
 
 var tracker = new JiraApi(
     config.protocol,
     config.host,
     config.port,
-    config.user,
+    config.username,
     config.password,
     config.api_version);
 
