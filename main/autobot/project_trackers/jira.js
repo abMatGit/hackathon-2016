6ea338c4cd9a056922dd9bb9491049a9bb537015
Alan Andrade <alan.andradec@gmail.com>
2016-07-20 23:43:25 -0700

    Project Tracker uses a client to interact with the outside world

--- main/autobot/project_trackers/jira.js
+++ main/autobot/project_trackers/jira.js
@@ -1,26 +1,17 @@
-var credentials = require('../../../configs/jira_credentials');
+var config = require('../../../configs/jira_credentials');
+
+var ProjectTracker = require('../autobot/project_trackers/project_tracker');
 var JiraApi = require('jira').JiraApi;
 
-var config = {
-  protocol: 'https',
-  host: credentials.host,
-  port: null,
-  username: credentials.username,
-  password: credentials.password,
-  api_version: 'latest'
-}
 
-module.exports.client = new JiraApi(config.protocol,
-                                    config.host,
-                                    config.port,
-                                    config.username,
-                                    config.password,
-                                    config.api_version);
+var Jira = Object.create(ProjectTracker);
 
-// Call this like:
-// var jira_client = require('project_trackers/jira').client;
-// var jira_callback = require('project_trackers/jira').status_issue_callback;
-//
-// Invoke it with:
-// jira_client.findIssue('IOS-1697', jira_callback);
-module.exports.status_issue_callback = function(err, issue) { console.log('Status: ' + issue.fields.status.name) };
+var tracker = new JiraApi(
+    config.protocol,
+    config.host,
+    config.port,
+    config.user,
+    config.password,
+    config.api_version);
+
+module.exports = new Jira(tracker);
--- test/projectTrackerTests.js
+++ test/projectTrackerTests.js
@@ -1,15 +1,21 @@
 var assert = require('chai').assert;
 var ProjectTracker = require('../main/autobot/project_tracker');
 
+var Client = {}; // TODO: flesh this out.
+var FakeClient = function () {
+    this.findIssue = function (story, handler) {
+        handler.ok(story);
+    };
+}
+
 describe('Project Tracker', function () {
     describe('getStory', function () {
-        xit('returns foo', function (done) {
-            this.timeout(50000);
+        it('returns the storyID on data', function (done) {
+            var client = new FakeClient();
+            var tracker = new ProjectTracker(client);
 
-            var jira = new ProjectTracker('jira');
-
-            jira.getStory('IOS-1383', function (err, data) {
-                assert.equal(data, 'foo');
+            tracker.getStory('IOS-1383', function (err, data) {
+                assert.equal(data, 'IOS-1383');
                 done();
             });
         });
