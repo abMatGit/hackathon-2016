abb022e59a3f0821d6cbb722b0ddf844ef70a391
abMatGit <amatuszewski@lumoslabs.com>
2016-07-25 19:38:50 -0700

    remove .js from files

--- main/autobot.js
+++ main/autobot.js
@@ -1,7 +1,7 @@
-var Handler = require('./autobot/handler.js');
-var access = require('./lib/resource_accessor.js').access;
-var core = require('./autobot/core/core.js');
-var Slack = require('./autobot/adapters/slack.js');
+var Handler = require('./autobot/handler');
+var access = require('./lib/resource_accessor').access;
+var core = require('./autobot/core/core');
+var Slack = require('./autobot/adapters/slack');
 
 // Adapters : Still a fresh idea since autobot can use many different adapters.
 var Adapters = {
